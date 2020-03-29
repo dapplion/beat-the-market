b4deee971c465ddd52ee725fc6016495e4556a14
dapplion <guillem.lion.benosa@gmail.com>
2020-03-29 05:12:33 +0200

    Updates

---
+++ precache-manifest.ab7b034b414890dd9cad7765f21eef52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aacd175d9c35e1dc87423fa3b773f628",
+    "url": "/beat-the-market/index.html"
+  },
+  {
+    "revision": "c29ada704f7fd55a4673",
+    "url": "/beat-the-market/static/css/main.1b6a2fe0.chunk.css"
+  },
+  {
+    "revision": "771bc4415bbf06e35033",
+    "url": "/beat-the-market/static/js/2.7416cf69.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/beat-the-market/static/js/2.7416cf69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c29ada704f7fd55a4673",
+    "url": "/beat-the-market/static/js/main.a4bd6b9f.chunk.js"
+  },
+  {
+    "revision": "f894730caa059d4a9419",
+    "url": "/beat-the-market/static/js/runtime-main.3aa8d487.js"
+  }
+]);
